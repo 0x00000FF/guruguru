7df1d55ecf17159064994d83009694eaed8548d0
Kangjun Heo == P.Knowledge <knowledge@o.cnu.ac.kr>
2020-01-04 22:58:24 +0900

    click-to-translate with papago

--- src/guruguru.js
+++ src/guruguru.js
@@ -7,6 +7,56 @@
  * 
  */
 
+const apiBase = (svc) => 
+    "https://openapi.naver.com/v1/papago/" + svc;
+
+
+let apiId  = "";
+let apiKey = "";
+
+let request = (method, url, data, done) => {
+    
+    var xhr = new XMLHttpRequest();
+
+    xhr.open(method, url);
+
+    xhr.setRequestHeader("X-Naver-Client-Id", apiId);
+    xhr.setRequestHeader("X-Naver-Client-Secret", apiKey);
+    xhr.setRequestHeader("Content-Type", "jsonp");
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === xhr.DONE) {
+            if (xhr.status === 200 || xhr.status === 201) {
+                done(xhr.responseText);
+            }
+        }
+    }
+
+    xhr.send(data);
+};
+
+let translate = (btn) => {
+    const txtElem = btn.toElement;
+
+    let srcLang = ""; 
+    let dstLang = "ja";
+    
+    request("POST", apiBase("detectLangs"), 
+            "query=" + txtElem.innerText,
+            (data) => {
+                srcLang = data.langCode;
+                request("POST", apiBase("n2mt"),
+                         "source=" + srcLang +
+                         "&target=" + dstLang +  
+                         "&text=" + txtElem.innerText,
+                         (data) => {
+                             txtElem.innerHTML = data.message
+                                                     .result
+                                                     .translatedText;
+                         });
+            });
+};
+
 (function () {
     const targetNode = document.querySelector("yt-live-chat-item-list-renderer #contents #item-scroller");
     const config = { attributes: false, childList: true, subtree: true };
@@ -24,7 +74,8 @@
             }
         }
 
-        console.log(extractTarget.innerText);
+        extractTarget.style = "cursor: pointer";
+        extractTarget.addEventListener("click", translate);
     };
 
     const chatObserver = new MutationObserver(callback);
